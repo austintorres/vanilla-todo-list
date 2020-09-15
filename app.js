f4e3eb67ca9e8519c25bbdac511b96a4324a1417
austintorres <torresgaustin@gmail.com>
2020-09-14 22:38:34 -0400

    Partial update

--- app.js
+++ app.js
@@ -11,72 +11,72 @@ filterOption.addEventListener('click', filterTodo);
 
 // FUNCTIONS
 function addTodo(event) {
-	// prevent form from submitting
-	event.preventDefault()
-	// Todo DIV
-	const todoDiv = document.createElement('div');
-	todoDiv.classList.add("todo");
-	// Create list item
-	const newTodo = document.createElement('li');
-	newTodo.innerText = todoInput.value;
-	newTodo.classList.add("todo-item");
-	todoDiv.appendChild(newTodo);
-	// Check mark button
-	const completedButton = document.createElement("button");
-	completedButton.innerHTML = '<i class="fas fa-check"></i>';
-	completedButton.classList.add('complete-btn');
-	todoDiv.appendChild(completedButton);
-	// Check trash button
-	const trashButton = document.createElement("button");
-	trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-	trashButton.classList.add('trash-btn');
-	todoDiv.appendChild(trashButton);
-	// Append to list
-	todoList.appendChild(todoDiv);
-	// Clear todo input value after submit
-	todoInput.value = "";
+    // prevent form from submitting
+    event.preventDefault()
+    // Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    // Create list item
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    // Check mark button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Check trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    // Clear todo input value after submit
+    todoInput.value = "";
 }
 
 function deleteCheck(event) {
-	const item = event.target;
-	// Delete todo
-	if (item.classList[0] === "trash-btn") {
-		const todo = item.parentElement;
-		// Animation
-		todo.classList.add("fall");
-		todo.addEventListener('transitionend', function () {
-			todo.remove();
-		});
-	}
+    const item = event.target;
+    // Delete todo
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        // Animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
+        });
+    }
 
-	// Check mark
-	if (item.classList[0] === "complete-btn") {
-		const todo = item.parentElement;
-		todo.classList.toggle("completed");
-	}
+    // Check mark
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
 
 function filterTodo(event) {
-	const todos = todoList.childNodes;
-	todos.forEach(function (todo) {
-		switch (event.target.value) {
-			case "all":
-				todo.style.display = "flex";
-				break;
-			case "completed":
-				if (todo.classList.contains('completed')) {
-					todo.style.display = 'flex';
-				} else {
-					todo.style.display = "none";
-				}
-				break;
-			case "incomplete":
-				if (!todo.classList.contains('completed')) {
-					todo.style.display = 'flex';
-				} else {
-					todo.style.display = "none";
-				}
-				break;
-		}
-	});
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (event.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "incomplete":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
 }
